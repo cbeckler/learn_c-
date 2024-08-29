2a5a23b2225830217bc3deb7c711c6ad073dfd14
Charlie Beckler <47010787+cbeckler@users.noreply.github.com>
2024-08-29 16:42:27 -0400

    initials program

---
+++ initials.cpp
@@ -0,0 +1,13 @@
+// program to write my initials
+#include <iostream>
+
+int main(){
+  std::cout << "  CCCCC             A             BBBBBB\n";
+  std::cout << " C     C          A   A           B      B\n";
+  std::cout << "C                A     A          B     B\n";
+  std::cout << "C               AAAAAAAAA         BBBBBB\n";
+  std::cout << "C              A         A        B      B\n";
+  std::cout << " C     C      A           A       B       B\n";
+  std::cout << "  CCCCC      A             A      BBBBBBBB\n";
+  return 0;
+}
