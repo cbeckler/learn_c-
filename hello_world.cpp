92e031541191a0db749d8e3acf539fc4e2f75a3a
Charlie Beckler <47010787+cbeckler@users.noreply.github.com>
2024-08-29 14:20:45 -0400

    hello world file

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+// This program demonstrates console text output
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+    std::cout << "I'm learning C++. \n";
+    std::cout << "This will make me a better programmer. \n";
+    return 0;
+}
